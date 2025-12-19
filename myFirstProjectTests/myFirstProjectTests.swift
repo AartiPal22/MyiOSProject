fe19f6bcb995f60ebdfb74620b616334ced8192e
Aarti Pal <aartipal@Aartis-MacBook-Air.local>
2025-12-19 11:56:48 +0530

    First Commit

---
+++ myFirstProjectTests/myFirstProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstProjectTests.swift
+//  myFirstProjectTests
+//
+//  Created by Aarti Pal on 19/12/25.
+//
+
+import Testing
+@testable import myFirstProject
+
+struct myFirstProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
