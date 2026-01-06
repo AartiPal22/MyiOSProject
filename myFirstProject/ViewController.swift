f84bba7c8b6f7eef65d6fdf681cf32efd2f38b2a
Aarti Pal <aartipal@Aartis-MacBook-Air.local>
2026-01-06 12:15:53 +0530

    Add home screen UI

--- myFirstProject/ViewController.swift
+++ myFirstProject/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("New Controller")
         // Do any additional setup after loading the view.
     }
 
